b6bf5914f93cf444f2c7f440f760dbb20c7aa28c
Cezar <fntblk@gmail.com>
2026-05-01 18:56:28 -0300

    vector boilerplate

--- src/utils.c
+++ src/utils.c
@@ -1,1 +1,17 @@
 #include "utils.h"
+
+extern Vec3 vec3AddVector(Vec3 a, Vec3 b) {
+    return (Vec3){
+        a.x + b.x,
+        a.y + b.y,
+        a.z + b.z,
+    };
+}
+
+extern Vec3 vec3AddScalar(Vec3 vec, float scalar) {
+    return (Vec3){
+        vec.x + scalar,
+        vec.y + scalar,
+        vec.z + scalar,
+    };
+}
--- src/utils.h
+++ src/utils.h
@@ -1,4 +1,24 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+typedef struct Vec3 {
+    float x;
+    float y;
+    float z;
+} Vec3;
+
+/**
+ * @brief Computes the sum of 2 vectors.
+ *
+ * @return Vector + Vector sum.
+ */
+extern Vec3 vec3AddVector(Vec3 a, Vec3 b);
+
+/**
+ * @brief Sums all vector components with a scalar.
+ *
+ * @return Vector + Scalar sum.
+ */
+extern Vec3 vec3AddScalar(Vec3 vec, float scalar);
+
 #endif /* UTILS_H */
