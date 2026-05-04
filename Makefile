e74b19984863fb686452bfd0a711856e9035fc32
Cezar <fntblk@gmail.com>
2026-05-04 15:21:53 -0300

    SDL3

--- Makefile
+++ Makefile
@@ -1,26 +1,36 @@
-.PHONY: clean
+.PHONY: all check clean run
 
 GL_CFLAGS    := $(shell pkg-config --cflags opengl)
-GLUT_CFLAGS  := $(shell pkg-config --cflags glut)
+SDL3_CFLAGS  := $(shell pkg-config --cflags sdl3)
 GLEW_CFLAGS  := $(shell pkg-config --cflags glew)
 GL_LDFLAGS   := $(shell pkg-config --libs opengl)
-GLUT_LDFLAGS := $(shell pkg-config --libs glut)
+SDL3_LDFLAGS := $(shell pkg-config --libs sdl3)
 GLEW_LDFLAGS := $(shell pkg-config --libs glew)
 
 CFLAGS       := -ansi -O2 -g -Wall -Wextra -Wpedantic -Wshadow -Wformat=2 -Werror
-CFLAGS       += $(GL_CFLAGS) $(GLUT_CFLAGS) $(GLEW_CFLAGS)
+CFLAGS       += $(GL_CFLAGS) $(SDL3_CFLAGS) $(GLEW_CFLAGS)
 LDFLAGS      := -lm
-LDFLAGS      += $(GL_LDFLAGS) $(GLUT_LDFLAGS) $(GLEW_LDFLAGS)
+LDFLAGS      += $(GL_LDFLAGS) $(SDL3_LDFLAGS) $(GLEW_LDFLAGS)
 
 BINARY       := space-game
 SOURCES      := $(wildcard src/*.c)
+HEADERS      := $(wildcard src/*.h)
 OBJECTS      := $(patsubst %.c,%.o,$(SOURCES))
 
+all: $(BINARY)
+
 $(BINARY): $(OBJECTS)
-	$(CC) $(LDFLAGS) $(OBJECTS) -o $(BINARY)
+	$(CC) $(OBJECTS) -o $(BINARY) $(LDFLAGS)
 
 src/%.o: src/%.c
-	$(CC) $(CFLAGS) -c $^ -o $@
+	$(CC) -c $^ -o $@ $(CFLAGS)
+
+check:
+	clang-tidy $(SOURCES) $(HEADERS)
+	clang-format -i $(SOURCES) $(HEADERS)
+
+run: all
+	./$(BINARY)
 
 clean:
 	rm -f $(BINARY) $(OBJECTS)
--- src/main.c
+++ src/main.c
@@ -1,3 +1,63 @@
+#include <GL/gl.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_video.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
 int main(void) {
+    SDL_Window* sdl_window;
+    SDL_Event sdl_event;
+    SDL_GLContext gl_context;
+    bool running = true;
+
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        return -1;
+    }
+
+    /* OpenGL 2.1 */
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+    sdl_window = SDL_CreateWindow("Space Game", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
+    if (sdl_window == NULL) return -1;
+
+    gl_context = SDL_GL_CreateContext(sdl_window);
+
+    while (running) {
+        while (SDL_PollEvent(&sdl_event)) {
+            if (sdl_event.type == SDL_EVENT_QUIT) {
+                running = false;
+            }
+        }
+
+        glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        glClearColor(0.1F, 0.1F, 0.1F, 1.0F);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* hello world triangle */
+        glBegin(GL_TRIANGLES);
+        {
+            glColor3f(1.0F, 0.0F, 0.0F);
+            glVertex2f(-0.5F, -0.5F);
+
+            glColor3f(0.0F, 1.0F, 0.0F);
+            glVertex2f(0.5F, -0.5F);
+
+            glColor3f(0.0F, 0.0F, 1.0F);
+            glVertex2f(0.0F, 0.5F);
+        }
+        glEnd();
+
+        SDL_GL_SwapWindow(sdl_window);
+    }
+
+    SDL_GL_DestroyContext(gl_context);
+    SDL_DestroyWindow(sdl_window);
+    SDL_Quit();
+
     return 0;
 }
