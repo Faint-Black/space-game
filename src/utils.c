78b96030e69ce2291ef79ef37b44637e0d9aab03
Cezar <fntblk@gmail.com>
2026-05-12 14:26:36 -0300

    vector functions

--- src/utils.c
+++ src/utils.c
@@ -1,4 +1,20 @@
 #include "utils.h"
+#include <assert.h>
+#include <math.h>
+
+extern Vec3 vec3(float x, float y, float z) {
+    Vec3 result;
+    result.x = x;
+    result.y = y;
+    result.z = z;
+    return result;
+}
+
+extern Vec3 vec3Null(void) {
+    Vec3 result;
+    result.x = result.y = result.z = 0.0F;
+    return result;
+}
 
 extern Vec3 vec3AddVector(Vec3 a, Vec3 b) {
     Vec3 result;
@@ -14,4 +30,89 @@ extern Vec3 vec3AddScalar(Vec3 vec, float scalar) {
     result.y = vec.y + scalar;
     result.z = vec.z + scalar;
     return result;
+}
+
+extern Vec3 vec3SubVector(Vec3 a, Vec3 b) {
+    Vec3 result;
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+    result.z = a.z - b.z;
+    return result;
+}
+
+extern Vec3 vec3SubScalar(Vec3 vec, float scalar) {
+    Vec3 result;
+    result.x = vec.x - scalar;
+    result.y = vec.y - scalar;
+    result.z = vec.z - scalar;
+    return result;
+}
+
+extern Vec3 vec3HadamardMulVector(Vec3 a, Vec3 b) {
+    Vec3 result;
+    result.x = a.x * b.x;
+    result.y = a.y * b.y;
+    result.z = a.z * b.z;
+    return result;
+}
+
+extern Vec3 vec3MulScalar(Vec3 vec, float scalar) {
+    Vec3 result;
+    result.x = vec.x * scalar;
+    result.y = vec.y * scalar;
+    result.z = vec.z * scalar;
+    return result;
+}
+
+extern Vec3 vec3HadamardDivVector(Vec3 a, Vec3 b) {
+    Vec3 result;
+    assert(b.x != 0.0F);
+    assert(b.y != 0.0F);
+    assert(b.z != 0.0F);
+    result.x = a.x / b.x;
+    result.y = a.y / b.y;
+    result.z = a.z / b.z;
+    return result;
+}
+
+extern Vec3 vec3DivScalar(Vec3 vec, float scalar) {
+    Vec3 result;
+    assert(scalar != 0.0F);
+    result.x = vec.x / scalar;
+    result.y = vec.y / scalar;
+    result.z = vec.z / scalar;
+    return result;
+}
+
+extern float vec3Reduce(Vec3 vec) {
+    return vec.x + vec.y + vec.z;
+}
+
+extern float vec3Magnitude(Vec3 vec) {
+    const double x2 = (vec.x * vec.x);
+    const double y2 = (vec.y * vec.y);
+    const double z2 = (vec.z * vec.z);
+    const double result = sqrt(x2 + y2 + z2);
+    return (float)result;
+}
+
+extern float vec3Dot(Vec3 a, Vec3 b) {
+    return vec3Reduce(vec3HadamardMulVector(a, b));
+}
+
+extern Vec3 vec3Cross(Vec3 a, Vec3 b) {
+    Vec3 result;
+    result.x = (a.y * b.z) - (a.z * b.y);
+    result.y = (a.z * b.x) - (a.x * b.z);
+    result.z = (a.x * b.y) - (a.y * b.x);
+    return result;
+}
+
+extern Vec3 vec3Normalize(Vec3 vec) {
+    return vec3DivScalar(vec, vec3Magnitude(vec));
+}
+
+extern float vec3Distance(Vec3 a, Vec3 b) {
+    const Vec3 delta = vec3SubVector(b, a);
+    return vec3Magnitude(delta);
 }
--- src/utils.h
+++ src/utils.h
@@ -8,17 +8,115 @@ typedef struct Vec3 {
 } Vec3;
 
 /**
+ * @brief Creates a new vector from individual components.
+ *
+ * @return Vector(x, y, z)
+ */
+extern Vec3 vec3(float x, float y, float z);
+
+/**
+ * @brief Creates a null vector.
+ *
+ * @return Vector(0, 0, 0)
+ */
+extern Vec3 vec3Null(void);
+
+/**
  * @brief Computes the sum of 2 vectors.
  *
- * @return Vector + Vector sum.
+ * @return Vector((a.x + b.x), (a.y + b.y), (a.z + b.z))
  */
 extern Vec3 vec3AddVector(Vec3 a, Vec3 b);
 
 /**
- * @brief Sums all vector components with a scalar.
+ * @brief Sums all vector components by a scalar.
  *
- * @return Vector + Scalar sum.
+ * @return Vector((x + scalar), (y + scalar), (z + scalar))
  */
 extern Vec3 vec3AddScalar(Vec3 vec, float scalar);
+
+/**
+ * @brief Computes the difference of 2 vectors.
+ *
+ * @return Vector((a.x - b.x), (a.y - b.y), (a.z - b.z))
+ */
+extern Vec3 vec3SubVector(Vec3 a, Vec3 b);
+
+/**
+ * @brief Subtracts all vector components by a scalar.
+ *
+ * @return Vector((x - scalar), (y - scalar), (z - scalar))
+ */
+extern Vec3 vec3SubScalar(Vec3 vec, float scalar);
+
+/**
+ * @brief Multiplies 2 vectors, element-wise.
+ *
+ * @return Vector((a.x * b.x), (a.y * b.y), (a.z * b.z))
+ */
+extern Vec3 vec3HadamardMulVector(Vec3 a, Vec3 b);
+
+/**
+ * @brief Multiplies all vector components by a scalar.
+ *
+ * @return Vector((x * scalar), (y * scalar), (z * scalar))
+ */
+extern Vec3 vec3MulScalar(Vec3 vec, float scalar);
+
+/**
+ * @brief Divides 2 vectors, element-wise. Throws error if any B component is zero.
+ *
+ * @return Vector((a.x / b.x), (a.y / b.y), (a.z / b.z))
+ */
+extern Vec3 vec3HadamardDivVector(Vec3 a, Vec3 b);
+
+/**
+ * @brief Divides all vector components by a scalar. Throws error if scalar is zero.
+ *
+ * @return Vector((x / scalar), (y / scalar), (z / scalar))
+ */
+extern Vec3 vec3DivScalar(Vec3 vec, float scalar);
+
+/**
+ * @brief Reduces a vector into a scalar (sum).
+ *
+ * @return x + y + z
+ */
+extern float vec3Reduce(Vec3 vec);
+
+/**
+ * @brief Computes the length/size/magnitude of a vector.
+ *
+ * @return ||vec||
+ */
+extern float vec3Magnitude(Vec3 vec);
+
+/**
+ * @brief Computes the dot product of two vectors.
+ *
+ * @return dot(A, B)
+ */
+extern float vec3Dot(Vec3 a, Vec3 b);
+
+/**
+ * @brief Computes the cross product of two vectors.
+ *
+ * @return cross(A, B)
+ */
+extern Vec3 vec3Cross(Vec3 a, Vec3 b);
+
+/**
+ * @brief Normalizes a vector.
+ *
+ * @return \frac{ vec }{ ||vec|| }
+ */
+extern Vec3 vec3Normalize(Vec3 vec);
+
+/**
+ * @brief Calculates the distance between two vectors.
+ *
+ * @return ||A->B||
+ */
+extern float vec3Distance(Vec3 a, Vec3 b);
 
 #endif /* UTILS_H */
